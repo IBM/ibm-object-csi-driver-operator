8d0660f8bffffe3115ba2676b6a85a6b2f7e4429
Ambika Nair <ambiknai@in.ibm.com>
2024-02-29 03:16:00 -0800

    Rename
    Signed-off-by: Ambika Nair <ambiknai@in.ibm.com>
    Signed-off-by: Ashima-Ashima1 <Ashima.Ashima1@ibm.com>

--- main.go
+++ main.go
@@ -109,11 +109,11 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "IBMObjectCSI")
 		os.Exit(1)
 	}
-	if err = (&controllers.FixStaleVolumeReconciler{
+	if err = (&controllers.RestoreStaleVolumeReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "FixStaleVolume")
+		setupLog.Error(err, "unable to create controller", "controller", "RestoreStaleVolume")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
