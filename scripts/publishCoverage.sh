ff40f930d064a50e20805dd22cc3fb7741f77396
Prachi Shivanand Anure <prachishivanandanure@Prachis-MacBook-Pro.local>
2025-12-15 20:56:43 +0530

    minor changes

--- scripts/publishCoverage.sh
+++ scripts/publishCoverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #******************************************************************************
+# Copyright 2025 IBM Corp.
+# Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
 #******************************************************************************
 #
